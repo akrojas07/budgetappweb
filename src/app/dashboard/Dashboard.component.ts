eac6156471c1bd6acfb8e0639ea88c3dfd747679
Anakaren <ak.rojas.07@gmail.com>
2020-11-19 14:37:10 -0800

    added pie chart functionality

--- src/app/dashboard/Dashboard.component.ts
+++ src/app/dashboard/Dashboard.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, IterableDiffers } from '@angular/core';
 import { GoalsService } from '../_services/goals.service';
+import { BudgetExpenseService } from '../_services/budgetExpense.service';
+import { BudgetSavingsService } from '../_services/budgetSavings.service';
+import { BudgetService } from '../_services/budget.service';
+import { BudgetIncomeService } from '../_services/budgetIncome.service';
 import {
   ChartOptions,
   ChartType,
@@ -10,9 +14,8 @@ import {
 import { Colors, Label, SingleDataSet } from 'ng2-charts';
 import { GoalsResponse } from '../_models/GoalsResponse';
 import { GoalsRequest } from '../_models/GoalsRequest';
-import { trimTrailingNulls } from '@angular/compiler/src/render3/view/util';
-import { identifierModuleUrl } from '@angular/compiler';
-import { Observable } from 'rxjs';
+import { isNgTemplate } from '@angular/compiler';
+
 
 @Component({
   selector: 'app-Dashboard',
@@ -28,25 +31,25 @@ export class DashboardComponent implements OnInit {
   noGoalsMessage: string;
 
   goalsList: GoalsRequest[] = [];
+  targetExpenseDataTotal: number;
+  targetSavingsDataTotal: number;
+  actualExpenseDataTotal = 0;
+  actualSavingsDataTotal = 0;
+  actualIncomeDataTotal = 0;
+  targetDataArray: number[][] = [[], []];
+  actualDataArray: number[] = [];
+  expenseArrayLabel: string[] = [];
+  savingsArrayLabel: string[] = [];
+  expenseArrayAmount: number[] = [];
+  savingsArrayAmount: number[] = [];
   userId: number;
 
-  @Output() goals =  new EventEmitter<GoalsRequest>();
+  @Output() goals = new EventEmitter<GoalsRequest>();
 
   public barChartLabels1: Label[] = ['Savings', 'Expenses'];
   public barChartType1: ChartType = 'bar';
   public barChartLegend1 = true;
   public barChartPlugins1 = [];
-  public barChartData1: ChartDataSets[] = [
-    {
-      data: [3, 5],
-      label: 'Target',
-    },
-    {
-      data: [5, 8],
-      label: 'Actual',
-    },
-  ];
-
   public barChartColors: Colors[] = [
     {
       backgroundColor: '#98d7c2',
@@ -59,16 +62,14 @@ export class DashboardComponent implements OnInit {
     responsive: true,
     scales: { yAxes: [{ ticks: { min: 0 } }] },
   };
+  public barChartData1: ChartDataSets[] = [{label: 'Target', data: [500, 800]}, {label: 'Actual', data: [500, 800]}];
+
 
   public pieChartOptions1: ChartOptions = {
     responsive: true,
   };
-  public pieChartLabels1: Label[] = [
-    ['Download', 'Sales'],
-    ['In', 'Store', 'Sales'],
-    'Mail Sales',
-  ];
-  public pieChartData1: SingleDataSet = [300, 500, 100];
+  public pieChartLabels1: Label[] = [];
+  public pieChartData1: SingleDataSet = [];
   public pieChartType1: ChartType = 'pie';
   public pieChartLegend1 = true;
   public pieChartPlugins1 = [];
@@ -88,17 +89,21 @@ export class DashboardComponent implements OnInit {
   public pieChartOptions2: ChartOptions = {
     responsive: true,
   };
-  public pieChartLabels2: Label[] = [
-    ['Download', 'Sales'],
-    ['In', 'Store', 'Sales'],
-    'Mail Sales',
-  ];
-  public pieChartData2: SingleDataSet = [300, 500, 100];
+  public pieChartLabels2: Label[] = [];
+  public pieChartData2: SingleDataSet = [];
   public pieChartType2: ChartType = 'pie';
   public pieChartLegend2 = true;
   public pieChartPlugins2 = [];
 
-  constructor(private goalsService: GoalsService) {}
+
+
+  constructor(
+    private goalsService: GoalsService,
+    private expenseService: BudgetExpenseService,
+    private savingsService: BudgetSavingsService,
+    private incomeService: BudgetIncomeService,
+    private budgetService: BudgetService
+  ) {}
 
   ngOnInit() {
     this.showErrorMessage = false;
@@ -106,18 +111,14 @@ export class DashboardComponent implements OnInit {
 
     this.userId = Number(localStorage.getItem('userId'));
     this.getGoalsList();
+    this.getActualData();
+    this.getIncomeData();
+    this.getTargetData();
 
-    // let data = [];
-    // for(let i =0; i < result; i++){
-    //   data.push(result[i])
-    // }
-
-    // let dataset1 =  { label :'sadsad', data : data};
-    // this.barChartData.push(dataset1);
   }
 
   getGoalsList(): void {
-    this.goalsList =[];
+    this.goalsList = [];
     this.goalsService.getGoals(this.userId).subscribe(
       (res) => {
         if (res !== null) {
@@ -134,7 +135,7 @@ export class DashboardComponent implements OnInit {
               StartDate: goal.startDate,
               EndDate: goal.endDate,
               TargetAmount: goal.targetAmount,
-              Progress: progress > 100 ? 100 : progress
+              Progress: progress > 100 ? 100 : progress,
             });
           }
         }
@@ -147,6 +148,52 @@ export class DashboardComponent implements OnInit {
     );
   }
 
+  getActualData(): void {
+    this.actualDataArray = [];
+    this.savingsService.getAllSavingsByUser(this.userId).subscribe((res) =>
+    {
+      res.forEach((item) =>
+      {
+        this.actualSavingsDataTotal += item.savingsAmount;
+        this.pieChartData2.push(item.savingsAmount);
+        this.pieChartLabels2.push(item.savingsType);
+      });
+      this.actualDataArray.push(this.actualSavingsDataTotal);
+    });
+
+    this.expenseService.getAllExpensesByUser(this.userId).subscribe((res) => {
+      res.forEach((item) =>
+      {
+        this.actualExpenseDataTotal += item.expenseAmount;
+        this.pieChartLabels1.push(item.expenseType);
+        this.pieChartData1.push(item.expenseAmount);
+      });
+      this.actualDataArray.push(this.actualExpenseDataTotal);
+      this.barChartData1[[1][0]].data = [this.actualSavingsDataTotal, this.actualExpenseDataTotal];
+    });
+  }
+
+  getTargetData(): void{
+    this.budgetService.getBudgetBreakdownByUser(this.userId).subscribe(
+      (res) =>
+      {
+        this.targetExpenseDataTotal = (res.expensesBreakdown / 100) * this.actualIncomeDataTotal;
+        this.targetSavingsDataTotal = (res.savingsBreakdown / 100) * this.actualIncomeDataTotal;
+        this.barChartData1[[0][0]].data = [this.targetSavingsDataTotal, this.targetExpenseDataTotal];
+      }
+    );
+  }
+
+  getIncomeData(): void
+  {
+    this.incomeService.getAllIncomeByUser(this.userId).subscribe(
+      (res) => {
+        res.forEach(item => this.actualIncomeDataTotal += item.amount);
+      }
+    );
+  }
+
+
   refresh(): void {
     window.location.reload();
   }
@@ -154,14 +201,12 @@ export class DashboardComponent implements OnInit {
   addGoal(event: GoalsRequest[]): void {
     this.goalsList = [];
 
-    for(const goal of event)
-    {
+    for (const goal of event) {
       this.goalsList.push(goal);
     }
 
     this.upsertGoals();
   }
-
 
   upsertGoals(): void {
     console.log(this.goalsList);
@@ -178,5 +223,4 @@ export class DashboardComponent implements OnInit {
       }
     );
   }
-
 }
